f4df6beea52ccb2826e7a9df28d1071ffd88d840
zhaohejing2017 <zhaohejing@zhj125.cn>
2017-07-28 09:12:17 +0800

    添加 banner图  接口

--- src/main/java/com/yrkj/model/Shuffling/ShufflingSearch.java
+++ src/main/java/com/yrkj/model/Shuffling/ShufflingSearch.java
@@ -6,5 +6,13 @@ import com.yrkj.model.core.SearchModel;
  * Created by 45425 on 2017/7/25.
  */
 public class ShufflingSearch extends SearchModel {
+    public Integer getCate() {
+        return cate;
+    }
 
+    public void setCate(Integer cate) {
+        this.cate = cate;
+    }
+
+    private  Integer cate;
 }
