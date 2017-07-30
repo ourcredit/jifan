0f81f98a8ae40032f38b1bd8ef77ec14e8c70d12
Barry <xuenianxiang@gmail.com>
2017-07-30 12:57:21 +0800

    获取 分类列表

--- src/main/java/com/yrkj/controller/UserProductController.java
+++ src/main/java/com/yrkj/controller/UserProductController.java
@@ -1,15 +1,16 @@
 package com.yrkj.controller;
 
 import com.yrkj.model.UserProduct.UserProductSearch;
+import com.yrkj.model.core.ActionResult;
 import com.yrkj.model.core.PageModel;
 import com.yrkj.model.core.SearchModel;
+import com.yrkj.service.CategoryService;
 import com.yrkj.service.UserProductService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -23,12 +24,20 @@ public class UserProductController {
     @Autowired
     private UserProductService userProductService;
 
+    @Autowired
+    private CategoryService categoryService;
+
     @ApiOperation(value = "获取精选列表",notes = "获取精选列表")
     @RequestMapping(value = "/perfectList", method = RequestMethod.POST)
     public PageModel perfectList(@RequestBody SearchModel model) {
         return userProductService.getPerfectList(model);
     }
 
+    @ApiOperation(value = "根据pid获取分类列表",notes = "根据pid获取分类列表")
+    @RequestMapping(value = "/getAllByPid", method = RequestMethod.POST)
+    public ActionResult getAllByPid(@RequestParam Long pid) {
+        return categoryService.getAllByPid(pid);
+    }
 
     @ApiOperation(value = "获取商品列表",notes = "获取商品列表")
     @RequestMapping(value = "/productList", method = RequestMethod.POST)
