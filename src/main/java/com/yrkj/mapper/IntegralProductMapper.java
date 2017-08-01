dee21688eb16d0b6f4e09f3b85327d5557de2798
zhaohejing2017 <zhaohejing@zhj125.cn>
2017-08-01 14:20:01 +0800

    修改积分商城接口

--- src/main/java/com/yrkj/mapper/IntegralProductMapper.java
+++ src/main/java/com/yrkj/mapper/IntegralProductMapper.java
@@ -9,7 +9,7 @@ import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 
 /**
- * Created by 45425 on 2017/8/1.
+ * Created by zhaohejing on 2017/8/1.
  */
 @Mapper
 public interface IntegralProductMapper {
