a0c493f1a06f6773983678f5aa942ad843c5b449
Barry <xuenianxiang@gmail.com>
2017-08-13 13:21:31 +0800

    轮播图查询 status条件

--- src/main/java/com/yrkj/model/Shuffling/ShufflingSearch.java
+++ src/main/java/com/yrkj/model/Shuffling/ShufflingSearch.java
@@ -6,6 +6,19 @@ import com.yrkj.model.core.SearchModel;
  * Created by 45425 on 2017/7/25.
  */
 public class ShufflingSearch extends SearchModel {
+
+    private  Integer cate;
+
+    private Integer status;
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
     public Integer getCate() {
         return cate;
     }
@@ -14,5 +27,4 @@ public class ShufflingSearch extends SearchModel {
         this.cate = cate;
     }
 
-    private  Integer cate;
 }
