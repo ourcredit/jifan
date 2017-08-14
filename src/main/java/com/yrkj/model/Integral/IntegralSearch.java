7d4593c7150afc7278c9c85d269d037f3e0b1274
Barry <xuenianxiang@gmail.com>
2017-08-14 09:30:06 +0800

    cate

--- src/main/java/com/yrkj/model/Integral/IntegralSearch.java
+++ src/main/java/com/yrkj/model/Integral/IntegralSearch.java
@@ -6,6 +6,7 @@ import com.yrkj.model.core.SearchModel;
  * Created by 45425 on 2017/8/1.
  */
 public class IntegralSearch extends SearchModel {
+
     public Integer getStatus() {
         return status;
     }
@@ -15,4 +16,14 @@ public class IntegralSearch extends SearchModel {
     }
 
     private Integer status;
+
+    private Integer cate;
+
+    public Integer getCate() {
+        return cate;
+    }
+
+    public void setCate(Integer cate) {
+        this.cate = cate;
+    }
 }
