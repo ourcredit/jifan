5ada8fef1cc0e2eeea9b2db309b9334e09d49761
Barry <xuenianxiang@gmail.com>
2017-08-16 15:32:26 +0800

    积分订单

--- src/main/java/com/yrkj/model/Integral/IntegralOrder.java
+++ src/main/java/com/yrkj/model/Integral/IntegralOrder.java
@@ -17,6 +17,10 @@ public class IntegralOrder extends BaseModel {
     private String  courier_company;//快递公司
     private String  courier_order;//快递单号
     private String courier_time;//快递时间
+    private Integer province_id;
+    private String province_name;
+    private Integer city_id;
+    private String city_name;
     private String address;
     private String receiver;
     private String phone;
@@ -54,6 +58,38 @@ public class IntegralOrder extends BaseModel {
 
     public void setOrder_from(Integer order_from) {
         this.order_from = order_from;
+    }
+
+    public Integer getProvince_id() {
+        return province_id;
+    }
+
+    public void setProvince_id(Integer province_id) {
+        this.province_id = province_id;
+    }
+
+    public String getProvince_name() {
+        return province_name;
+    }
+
+    public void setProvince_name(String province_name) {
+        this.province_name = province_name;
+    }
+
+    public Integer getCity_id() {
+        return city_id;
+    }
+
+    public void setCity_id(Integer city_id) {
+        this.city_id = city_id;
+    }
+
+    public String getCity_name() {
+        return city_name;
+    }
+
+    public void setCity_name(String city_name) {
+        this.city_name = city_name;
     }
 
     public String getAddress() {
